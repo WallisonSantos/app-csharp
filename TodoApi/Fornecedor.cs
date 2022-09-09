ce47c50476caeac50020aa40df1de6f5e263c70e
WallisonSantos <wallisontiagoassis206@outlook.com>
2022-09-09 08:00:10 -0300

    First Commit
    Comitando ...

--- TodoApi/Fornecedor.cs
+++ TodoApi/Fornecedor.cs
@@ -3,38 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace TodoApi.Models {
-   public class Fornecedor {
-      private Guid _id;
-      private string? _nome;
-      private string? _documento;
-      private bool _ativo;
+namespace TodoApi
+{
+   public class Fornecedor
+   {
+      /* Atributos Privados, com implementação */
+      private string? _nroDeAcesso;
 
-      /*
-      public void AlteraNome(string nome) {
-         if(nome != null && nome.Length >= 2) {
-            _nome = nome;
-         }
+      /*Atributos autoimplementados: */
+      public Guid Id { get; private set; }
+      public bool Status { get; private set; }
+      public string? UserName { get; private set; }
+      public int Email { get; private set; }
+      public double Limite { get; private set; }
+      public double ValorCompra { get; private set; }
+
+      /* Construtores padrão */
+      public Fornecedor() { }
+      public Fornecedor(string username, bool status, int email)
+      {
+         Status = status;
+         UserName = username;
+         Email = email;
       }
-      */
 
-      public string Nome {
-         get { return _nome; }
-         set {
-            if(value != null && value.Length >= 2) {
-               _nome = value;
+      /* Implementações da classe */
+      public string? NroDeAcesso
+      {
+         get { return _nroDeAcesso; }
+         set
+         {
+            if (value != null && value.Length >= 5)
+            {
+               _nroDeAcesso = value;
             }
          }
       }
-
-      public string Documento {
-         get { return _documento; }
-         set {
-            if(value != null && value.Length >= 2) {
-               _documento = value;
-            }
+      public void EfetuarCompra(double vlr_compra)
+      {
+         if (Status == true && Limite > 0)
+         {
+            ValorCompra = ValorCompra + vlr_compra;
+            Limite = Limite - vlr_compra;
          }
       }
+
+      /* Retorno formatado ao usuário */
+      public override string ToString()
+      {
+         return "Olá " + UserName + " número gerado com sucesso " + Id + " seu limite de compras é de R$ " + Limite;
+      }
+
+
 
    }
 }
